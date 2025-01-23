1aaece6076dd96e3c43f0c1cafb3d260d2e394d3
Madumarov Davron <menaceteacher@gmail.com>
2025-01-23 22:21:42 +0500

    add

--- src/model/Coach.java
+++ src/model/Coach.java
@@ -2,8 +2,14 @@ package model;
 
 public class Coach extends User {
     private String specialization;
-    private int experienceYears;
-    private double rating;
-    
+    private Integer experienceYears;
+    private Double rating;
+
+    public Coach(Integer id, String name, String email, String password, String phoneNumber, String role, String specialization, Integer experienceYears, Double rating) {
+        super(id, name, email, password, phoneNumber, role);
+        this.specialization = specialization;
+        this.experienceYears = experienceYears;
+        this.rating = rating;
+    }
 
 }
