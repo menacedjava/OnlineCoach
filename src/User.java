f84f4ac8dfce2d87183dc1a0a84ea9f4b9a37ce4
Madumarov Davron <menaceteacher@gmail.com>
2025-01-23 22:15:22 +0500

    add

--- src/User.java
+++ src/User.java
@@ -1,36 +1,3 @@
 public class User {
-    private String name;
-    private int age;
 
-    public User() {
-    }
-
-    public User(int age, String name) {
-        this.age = age;
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
