a049304ac7963d64b44985bc5ed42e8370d15c59
Madumarov Davron <menaceteacher@gmail.com>
2025-01-24 22:00:00 +0500

    add

---
+++ src/model/TrainingProgram.java
@@ -0,0 +1,17 @@
+package model;
+
+public class TrainingProgram {
+
+        private Integer programId;
+        private Integer coachId;
+        private String name;
+        private String description;
+        private Integer duration; // in days
+        private String difficultyLevel;
+
+
+
+
+    }
+
+
