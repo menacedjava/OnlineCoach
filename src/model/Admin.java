959165766acd3e8e3363e5206537e4862b50b0b2
Madumarov Davron <menaceteacher@gmail.com>
2025-01-27 00:10:05 +0500

    add

--- src/model/Admin.java
+++ src/model/Admin.java
@@ -1,6 +1,12 @@
 package model;
 
 public class Admin extends User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private boolean active;
 
 
     
