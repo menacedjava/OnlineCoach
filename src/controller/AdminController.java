dcc202141e738535fd2841f7a7794d25ed01c2b1
Madumarov Davron <menaceteacher@gmail.com>
2025-01-28 21:47:54 +0500

    add

--- src/controller/AdminController.java
+++ src/controller/AdminController.java
@@ -1,12 +1,18 @@
 package controller;
 
+import model.Admin;
 import service.AdminService;
+import utils.Result;
 
 public class AdminController {
     private final AdminService adminService;
 
     public AdminController(AdminService adminService) {
         this.adminService = adminService;
+    }
+    public Result addAdmin(Admin admin) {
+        String admin1 = adminService.createAdmin(admin);
+        return new Result(true, admin1);
     }
 
 }
--- src/utils/Result.java
+++ src/utils/Result.java
@@ -3,4 +3,36 @@ package utils;
 public class Result {
     private boolean status;
     private String message;
+
+    public Result(boolean status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public Result() {
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "status=" + status +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
