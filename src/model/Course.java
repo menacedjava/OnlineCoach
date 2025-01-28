62cd0dd2c09bf7f15300a06ce1c5b980805da8a9
Madumarov Davron <menaceteacher@gmail.com>
2025-01-29 02:28:05 +0500

    add

--- src/model/Course.java
+++ src/model/Course.java
@@ -1,4 +1,60 @@
 package model;
 
 public class Course {
+    private Integer id;
+    private String name;
+    private String description;
+    private Double price;
+
+    public Course(Integer id, String name, String description, Double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Course() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
